9cb2813c16e20747f307efa2375c5b79d131ef10
Nathalia Gomez <natygomez2002@gmail.com>
2024-08-17 11:41:28 -0400

    Fixed Room and Server
    A bunch of our previously finished features did not work. probably because of the branch being out of date. made some changes to ensure everything works.

--- memeWorthy/app/server.js
+++ memeWorthy/app/server.js
@@ -147,7 +147,8 @@ function startRound2(roomId) {
     console.log(`Starting Round 2 for Room ${roomId}`);
     rooms[roomId].gameState.round = 2;
     rooms[roomId].gameState.submittedUsers = []; // Reset submitted users for the new round
-    io.to(roomId).emit('start-round', { round: 2, prompt: prompts[1] });
+    //io.to(roomId).emit('start-round', { round: 2, prompt: prompts[1] }); //Rabib look at this
+    io.to(roomId).emit('game-started', { round: 2, prompt: prompts[1] });
 }
 
 // Handle Round 2 Submissions
@@ -309,3 +310,6 @@ io.on('connection', (socket) => {
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
+// server.listen(port, () => {
+//     console.log(`Server is running on http://localhost:${port}`);
+// });
